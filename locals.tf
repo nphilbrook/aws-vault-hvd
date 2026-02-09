827390e3006d526a5abf81a204d2c58a85fa6ef1
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-02-09 09:14:36 -0500

    Explicit depends_on for the secrets so nodes don't spni up without secrets >:o

--- locals.tf
+++ locals.tf
@@ -1,19 +1,19 @@
 locals {
   primary_region = "us-west-2"
   common_tags = {
-    App                = "vault"
+    App                = "vault5"
     Env                = "sbx"
     Owner              = "nick.philbrook@hashicorp.com"
     "created-by"       = "terraform"
     "source_workspace" = var.TFC_WORKSPACE_SLUG
   }
 
-  vault_fqdn         = "vault.nick-philbrook.sbx.hashidemos.io"
-  vault_primary_fqdn = "vault-primary.nick-philbrook.sbx.hashidemos.io"
-  vault_dr           = "vault-dr.nick-philbrook.sbx.hashidemos.io"
-  vault_pr           = "vault-pr.nick-philbrook.sbx.hashidemos.io"
-  vault_whatever     = "vault-legacy.nick-philbrook.sbx.hashidemos.io"
-  vault_foo          = "vault-foo.nick-philbrook.sbx.hashidemos.io"
+  vault_fqdn         = "vault5.nick-philbrook.sbx.hashidemos.io"
+  vault_primary_fqdn = "vault5-primary.nick-philbrook.sbx.hashidemos.io"
+  vault_dr           = "vault5-dr.nick-philbrook.sbx.hashidemos.io"
+  vault_pr           = "vault5-pr.nick-philbrook.sbx.hashidemos.io"
+  vault_whatever     = "vault5-legacy.nick-philbrook.sbx.hashidemos.io"
+  vault_foo          = "vault5-foo.nick-philbrook.sbx.hashidemos.io"
 
   it_me         = data.aws_iam_session_context.human.issuer_arn
   r53_zone      = "nick-philbrook.sbx.hashidemos.io"
