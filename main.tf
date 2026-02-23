9078401681f261689b5107d2eddcae78085afd31
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-02-23 13:10:21 -0500

    drop PR cluster back to 6 nodes

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ module "vault_hvd_pr" {
   #------------------------------------------------------------------------------
   vm_key_pair_name = local.key_pair_name
   vm_instance_type = "t3a.medium"
-  asg_node_count   = 12
+  asg_node_count   = 6
   vm_image_id      = data.aws_ami.hc_base_rhel9_e2.id
   ec2_os_distro    = "rhel"
 
